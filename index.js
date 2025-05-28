0bd7482c24e62a120c2579cc907b9c5225cf48ce
Sergey Somov <letoka27@gmail.com>
2025-05-28 04:35:34 +0400

    remerge

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 console.log('Hello, World!');
 
 
-<<<<<<< HEAD
 const userAge = 36;
 
 if (userAge >= 18) {
-=======
-const age = 20;
-if(age >= 18) {
->>>>>>> future/user
     console.log('Вы совершеннолетний');
 } else {
     console.log('Вы НЕ совершеннолетний');
